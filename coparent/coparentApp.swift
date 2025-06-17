b94fe6ecec274745556bfa2d18cf6286b493ee91
T <numit22@gmail.com>
2025-06-17 13:55:13 +0200

    restructuring

--- coparent/coparentApp.swift
+++ coparent/coparentApp.swift
@@ -1,32 +1,68 @@
-//
-//  coparentApp.swift
-//  coparent
-//
-//  Created by T on 6/17/25.
-//
-
 import SwiftUI
-import SwiftData
+
+@Observable
+class AppState {
+    var isOnboarded = false
+    var currentUser: User?
+    var onboardingStep: OnboardingStep = .welcome
+    
+    enum OnboardingStep {
+        case welcome
+        case userType
+        case basicInfo
+        case terms
+        case complete
+    }
+    
+    func initializeServices() async {
+        do {
+            try await SendbirdService.shared.initialize()
+        } catch {
+            print("Failed to initialize Sendbird: \(error)")
+        }
+    }
+    
+    func connectToSendbird() async {
+        guard let user = currentUser else { return }
+        
+        do {
+            try await SendbirdService.shared.connect(userId: user.id, nickname: user.name)
+        } catch {
+            print("Failed to connect to Sendbird: \(error)")
+        }
+    }
+}
+
+struct User {
+    var id: String
+    var name: String
+    var userType: UserType
+    var email: String?
+    var phoneNumber: String?
+    
+    enum UserType {
+        case singleParent
+        case coParent
+        case potentialCoParent
+    }
+}
 
 @main
-struct CoparentApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+struct CoParentApp: App {
+    @State private var appState = AppState()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            if !appState.isOnboarded {
+                OnboardingView()
+                    .environment(appState)
+            } else {
+                MainTabView()
+                    .environment(appState)
+            }
         }
-        .modelContainer(sharedModelContainer)
+        .task {
+            await appState.initializeServices()
+        }
     }
 }
